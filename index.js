8c50ef46925563c7db4f579720cb5bf685fff8a2
fokudon395-boop <fokudon395@gmail.com>
2026-04-30 12:21:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ global.nicknameLockStatus = {};
 global.activeTargets1 = new Set();
 global.activeTargets2 = new Set();
 
-const OWNER_ID = "61562981052508";
+const OWNER_ID = "61574301767068";
 const appStateFiles = ["appstate1.json", "appstate2.json"];
 
 function startInstance(stateFile, idNum) {
